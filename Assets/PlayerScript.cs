cfe1c2cffbd0f23dfbd7b5a060e83d63848fe17a
Dylan M <dylanmcgrath.sia@gmail.com>
2024-08-05 14:35:49 -0400

    //almsot final build :)

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -9,6 +9,25 @@ public class PlayerScript : MonoBehaviour
     public GameObject FridgeButton;
     public GameObject BombButton;
     public GameObject BombFridgeButton;
+    public GameObject FirePlaceButton;
+    public GameObject LightSwitchONButton;
+    public GameObject LightswitchOFFButton;
+    public GameObject EnergyButton;//buy in shop;
+    public GameObject WaterButton; //buy in shop;
+    public GameObject CatButton;
+    public GameObject eighttext;
+    public GameObject fivetext;
+
+    public GameObject KitchenBG;
+    public GameObject LivingRoomBG; //maybe add a version on fire later on?
+    public GameObject BedroomBGLight;
+    public GameObject BedroomBGDark;
+    public GameObject ShopBG;
+
+    public bool InbedRoom;
+    public bool InLivingRoom;
+    public bool InKitchen;
+    public bool InShop;
 
     public float Hunger;
     public bool IsEating;
@@ -33,12 +52,14 @@ public class PlayerScript : MonoBehaviour
     public bool HasMoved;
 
     public bool PetCat;
-    public bool CatDelay;  //WORK ON CAT TIMER DELAY AFTER PETTING CAT AND THEN MOVE ON TO BUTTONS AND UI MECHANICS TO MAKE GAME WORK!!!
+    public bool CatDelay; 
     public float CatDelayTimer;
 
     public bool YouDied;
     public bool YouWin;
     public float WinTimer;
+
+    public float Money;
 
     public bool UsedWater;
 
@@ -72,33 +93,35 @@ public class PlayerScript : MonoBehaviour
         FireDelay = false;
         FireDelayTimer = 0;
 
-        BombFridgeButton.SetActive(false);
+        Money = 0;
+
+        InKitchen = true;
     }
-    
+
 
     // Update is called once per frame
     void Update()
     {
         WinTimer += 1 * Time.deltaTime;
 
-        if(IsEating == false)
+        if (IsEating == false)
         {
             Hunger -= 1 * Time.deltaTime; //when ur eating from the fridge, it replenishes hunger very slowly, but there is unlimited food
         }
-        if (Hunger <=0)
+        if (Hunger <= 0)
         {
             YouDied = true;
         }
-        if(IsEating == true)
+        if (IsEating == true)
         {
             Hunger += 2 * Time.deltaTime;
         }
 
-        if(IsFridged == false) //bomb time can't be undone, however it can be indefinitely stalled by keeping it in the fridge, so long as you dont need to eat
+        if (IsFridged == false) 
         {
             BombTime -= 1 * Time.deltaTime;
 
-            if(CatNumber == 1 && IsCatAngry == true)
+            if (CatNumber == 1 && IsCatAngry == true)
             {
                 BombTime -= 2 * Time.deltaTime;
             }
@@ -107,11 +130,15 @@ public class PlayerScript : MonoBehaviour
         {
             YouDied = true;
         }
-
-        if(IsBurning == true && FireDelay == false) // when fire is splashed with water, fire is delayed for a few seconds , and loses like 20 to 30 on progress.
+        if (Hunger >= 100) 
         {
-            FireSpread += 1 * Time.deltaTime; 
-           
+            Hunger = 100;
+        }
+
+        if (IsBurning == true && FireDelay == false) // loses like 20 to 30 on progress when splashed wityh water
+        {
+            FireSpread += 1 * Time.deltaTime;
+
             if (CatNumber == 2 && IsCatAngry == true)
             {
                 FireSpread += 2 * Time.deltaTime;
@@ -121,11 +148,11 @@ public class PlayerScript : MonoBehaviour
         {
             FireDelay = true;
         }
-        if(FireDelay == true)
+        if (FireDelay == true)
         {
             FireDelayTimer += 1 * Time.deltaTime;
         }
-        if(FireDelayTimer >= 5)
+        if (FireDelayTimer >= 5)
         {
             FireDelay = false;
             FireDelayTimer = 0;
@@ -134,17 +161,21 @@ public class PlayerScript : MonoBehaviour
         {
             YouDied = true;
         }
-        if(UsedWater == true)
+        if (UsedWater == true)
         {
             FireSpread -= 20;
             FireDelay = true;
             Water -= 1;
             UsedWater = false;
         }
-
-        if(LightsOn == false)
+        if (FireSpread <= 0) //prevents firespread from going below zero
         {
-            MonsterTime += 1 * Time.deltaTime; 
+            FireSpread = 0;
+        }
+
+        if (LightsOn == false)
+        {
+            MonsterTime += 1 * Time.deltaTime;
 
             if (CatNumber == 3 && IsCatAngry == true)
             {
@@ -161,65 +192,282 @@ public class PlayerScript : MonoBehaviour
 
             Energy -= 1 * Time.deltaTime;
         }
-        if(MonsterTime >= 35)
+        if (MonsterTime >= 35)
         {
             YouDied = true;
         }
-        if (Energy <= 0)
-        {
-            LightsOn = false;
-        }
-       
+        
 
-
-        if(IsCatAngry == false)
+        if (IsCatAngry == false)
         {
             CatTemper -= 1 * Time.deltaTime;
         }
         else
         {
 
-            if(HasMoved == false)
+            if (HasMoved == false)
             {
                 CatNumber = Random.Range(1, 4);
                 HasMoved = true;
             }
         }
-        if(CatTemper <= 0)
+        if (CatTemper <= 0)
         {
             IsCatAngry = true;
             CatTemper = 0;
         }
 
+        if(Energy <=0)
+        {
+            LightsOn = false;
+            Energy = 0;
+
+            if(InbedRoom  == true)
+            {
+                BedroomBGDark.SetActive(true);
+                LightswitchOFFButton.SetActive(true);
+            }
+        }
 
 
-        if(WinTimer >= 300)
+        if(CatNumber == 1 && InKitchen == true) 
+        {
+            CatButton.SetActive(true);
+        }
+        else
+        {
+            CatButton.SetActive(false);
+        }
+
+
+        if (CatNumber == 2 && InLivingRoom == true) 
+        {
+            CatButton.SetActive(true);
+        }
+       
+
+
+        if (CatNumber == 3 && InbedRoom == true)
+        {
+            CatButton.SetActive(true);
+        }
+        
+
+
+        if (WinTimer >= 300)
         {
             YouWin = true;
         }
-        //losing conditions down here (maybe i can combine all of them into one if statement combininbg when fire is more than 70 and monster more than 35 maybe and the rest exept cat are less than zero, idk ask the teachers or sumthing.
+        //losing conditions down here 
+
+    }
+        public void Fridge()
+        {
+            Hunger += 3;
+            Debug.Log("Player Ate from Fridge");
+        }
+        public void Bomb()
+        {
+            IsFridged = true;
+            BombFridgeButton.SetActive(true);
+            BombButton.SetActive(false);
+            FridgeButton.SetActive(false);
+            Debug.Log("FRidgebomb should be here now");
+
+        }
+        public void BombFridge()
+        {
+            IsFridged = false;
+            Debug.Log("test for bomb fridge destruction");
+            BombButton.SetActive(true);
+            FridgeButton.SetActive(true);
+            BombFridgeButton.SetActive(false);
+        }
+
+        public void FirePlace()
+        {
+            if (Water >= 1)
+            {
+                UsedWater = true;
+            }
+        }
+
+        public void SwitchUp() // the button u can press when the ligths are ON
+        {
+            LightsOn = false;
+            LightSwitchONButton.SetActive(false);
+            LightswitchOFFButton.SetActive(true);
+            BedroomBGLight.SetActive(false);
+            BedroomBGDark.SetActive(true);
+        }
+
+        public void SwitchDown()
+        {
+            if(Energy >= 0.01)
+            {
+                LightsOn = true;
+                LightSwitchONButton.SetActive(true);
+                LightswitchOFFButton.SetActive(false);
+                BedroomBGLight.SetActive(true);
+                BedroomBGDark.SetActive(false);
+            }
+            
+        }
+
+        public void Bedroom()
+        {
+            if(LightsOn == true)
+            {
+                LightSwitchONButton.SetActive(true);
+                LightswitchOFFButton.SetActive(false);
+                BedroomBGLight.SetActive(true);
+                BedroomBGDark.SetActive(false);
+                
+            }
+            else
+            {
+                LightSwitchONButton.SetActive(false);
+                LightswitchOFFButton.SetActive(true);
+                BedroomBGLight.SetActive(false);
+                BedroomBGDark.SetActive(true);
+
+                 
+            }
+
+            InbedRoom = true;
+            InLivingRoom = false;
+            InKitchen = false;
+            InShop = false;
+
+            FridgeButton.SetActive(false);
+            BombFridgeButton.SetActive(false);
+            BombButton.SetActive(false);
+            KitchenBG.SetActive(false);
+            LivingRoomBG.SetActive(false);
+            FirePlaceButton.SetActive(false);
+            ShopBG.SetActive(false);
+            WaterButton.SetActive(false);
+            EnergyButton.SetActive(false);
+            fivetext.SetActive(false);
+            eighttext.SetActive(false);
+        }
+
+        public void LivingRoom()
+        {
+            LivingRoomBG.SetActive(true);
+            FirePlaceButton.SetActive(true);
+            
+            
+            InbedRoom = false;
+            
+            FridgeButton.SetActive(false);
+            BombFridgeButton.SetActive(false);
+            BombButton.SetActive(false);
+            KitchenBG.SetActive(false);
+            LightSwitchONButton.SetActive(false);
+            LightswitchOFFButton.SetActive(false);
+            BedroomBGLight.SetActive(false);
+            BedroomBGDark.SetActive(false);
+            ShopBG.SetActive(false);
+            WaterButton.SetActive(false);
+            EnergyButton.SetActive(false);
+            fivetext.SetActive(false);
+            eighttext.SetActive(false);
+
+        InLivingRoom = true;
+            InbedRoom = false;
+            InKitchen = false;
+            InShop = false;
+        }
+
+        public void Kitchen()
+        {
+            KitchenBG.SetActive(true);
+            if (IsFridged == true)
+            {
+               BombFridgeButton.SetActive(true);
+               
+            }
+
+            else
+            {
+               BombButton.SetActive(true);
+               FridgeButton.SetActive(true);
+
+               
+            }
+
+           InLivingRoom = false;
+           InKitchen = true;
+           InbedRoom = false;
+           InShop = false;
+
+            LivingRoomBG.SetActive(false);
+            FirePlaceButton.SetActive(false);
+            LightSwitchONButton.SetActive(false);
+            LightswitchOFFButton.SetActive(false);
+            BedroomBGLight.SetActive(false);
+            BedroomBGDark.SetActive(false);
+            ShopBG.SetActive(false);
+            WaterButton.SetActive(false);
+            EnergyButton.SetActive(false);
+            fivetext.SetActive(false);
+            eighttext.SetActive(false);
+
+        }
+
+    public void Cat ()
+    {
+        CatTemper += 6;
+        IsCatAngry = false;
+        HasMoved = false;
+        Money += 1;
     }
 
-   public void Fridge()
+    public void Shop ()
     {
-        Hunger += 3;
-        Debug.Log("Player Ate from Fridge");
-    }
-    public void Bomb()
-    {
-        IsFridged = true;
-        Instantiate(BombFridgeButton, FridgeButton.transform);
-        Destroy(BombButton);
-        Destroy(FridgeButton);
-        Debug.Log("FRidgebomb should be here now");
-    }
-    public void BombFridge()
-    {
-        IsFridged = false;
-        Debug.Log("test for bomb fridge destruction");
-        Instantiate(BombButton);
-        Instantiate(FridgeButton);
-        Destroy(BombFridgeButton);
+        ShopBG.SetActive(true);
+        WaterButton.SetActive(true);
+        EnergyButton.SetActive(true);
+        fivetext.SetActive(true);
+        eighttext.SetActive(true);
+        
+        InShop = true;
+        InKitchen = false;
+        InbedRoom = false;
+        InLivingRoom = false;
+
+        FridgeButton.SetActive(false);
+        BombFridgeButton.SetActive(false);
+        BombButton.SetActive(false);
+        KitchenBG.SetActive(false);
+        LivingRoomBG.SetActive(false);
+        FirePlaceButton.SetActive(false);
+        LightSwitchONButton.SetActive(false);
+        LightswitchOFFButton.SetActive(false);
+        BedroomBGLight.SetActive(false);
+        BedroomBGDark.SetActive(false);
     }
 
+    public void WateraButton ()
+    {
+        if(Money >= 5)
+        {
+            Money -= 5;
+            Water += 1;
+        }
+
+    }
+
+    public void EnergyaButton ()
+    {
+        if(Money >= 8)
+        {
+            Money -= 8;
+            Energy += 1;
+        }
+    }
+
+        
+ 
 }
